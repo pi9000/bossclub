e95b32a13d8c37e061cfc689dd3953872a419d4b
pi9000 <revify99@gmail.com>
2025-08-20 21:01:12 +0700

    Update GameController.php

--- core/app/Http/Controllers/Frontend/GameController.php
+++ core/app/Http/Controllers/Frontend/GameController.php
@@ -210,11 +210,10 @@ class GameController extends Controller
 
             $seamlesWsController = new SeamlesWsController();
             $result = $seamlesWsController->huidu_launch_game($user_code, $game_code, $balance);
-            return $result;
             if ($result->code == 0) {
                 return redirect($result->payload->game_launch_url);
             } else {
-                return $result;
+                return back()->with('error', $result->msg);
             }
         }
     }
